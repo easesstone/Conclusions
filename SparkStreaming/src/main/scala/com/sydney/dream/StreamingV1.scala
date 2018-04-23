269e9682d5906ca76efcbae3002a53290f055d03
unnunique <947538642@qq.com>
2018-04-23 17:22:55 +0800

    spark streaming checkpoint...

--- SparkStreaming/src/main/scala/com/sydney/dream/StreamingV1.scala
+++ SparkStreaming/src/main/scala/com/sydney/dream/StreamingV1.scala
@@ -12,20 +12,24 @@ import org.apache.spark.streaming.kafka.KafkaUtils
 
 object StreamingV1 {
     def main(args: Array[String]): Unit = {
+        val ssc = StreamingContext.getOrCreate("/checkpoint", getStreamingContext _)
+        ssc.start()
+        ssc.awaitTermination()
+    }
+
+    def getStreamingContext() : StreamingContext = {
         val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreamingOnKafkaDirect")
         val ssc = new StreamingContext(conf, Durations.seconds(15))
-        ssc.checkpoint("/checkpoint")
         val kafkaParams = Map(
-            "metadata.broker.list" -> "172.18.18.100:9092,172.18.18.101:9092,172.18.18.102:9092",
-            "group.id" -> "DemoGroup"
+        "metadata.broker.list" -> "172.18.18.100:9092,172.18.18.101:9092,172.18.18.102:9092",
+        "group.id" -> "DemoGroup"
         )
         val topicsSet = "first".split(",").toSet
         val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
         messages.foreachRDD(rdd => {
             rdd.foreach(row => {println("key: " + row._1 + ", "  + "value: " + row._2)})
         })
-
-        ssc.start()
-        ssc.awaitTermination()
+        ssc.checkpoint("/checkpoint")
+        ssc
     }
 }
