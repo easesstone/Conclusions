7b94db9aa28d1c3a719a3de954af8ea9e103f254
easesstone <576395455@qq.com>
2018-04-23 16:03:22 +0800

    spark-streaming demo 程序，不保存kafka offset...

--- SparkStreaming/src/main/scala/com/sydney/dream/StreamingV1.scala
+++ SparkStreaming/src/main/scala/com/sydney/dream/StreamingV1.scala
@@ -14,10 +14,10 @@ object StreamingV1 {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreamingOnKafkaDirect")
         val ssc = new StreamingContext(conf, Durations.seconds(15))
-        ssc.checkpoint("checkpoint")
+        ssc.checkpoint("/checkpoint")
         val kafkaParams = Map(
             "metadata.broker.list" -> "172.18.18.100:9092,172.18.18.101:9092,172.18.18.102:9092",
-            "group.id" -> "FaceObjectConsumerGroup"
+            "group.id" -> "DemoGroup"
         )
         val topicsSet = "first".split(",").toSet
         val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
