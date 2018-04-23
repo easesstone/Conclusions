1c5ccde8d68e3427a16b68c0a37d4d823fe2b9fa
unnunique <947538642@qq.com>
2018-04-23 14:37:45 +0800

    Spark Streaming。......

--- SparkStreaming/src/main/scala/com/sydney/dream/StreamingV1.scala
+++ SparkStreaming/src/main/scala/com/sydney/dream/StreamingV1.scala
@@ -14,25 +14,18 @@ object StreamingV1 {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreamingOnKafkaDirect")
         val ssc = new StreamingContext(conf, Durations.seconds(15))
-        ssc.checkpoint("/checkpoint")
+        ssc.checkpoint("checkpoint")
         val kafkaParams = Map(
             "metadata.broker.list" -> "172.18.18.100:9092,172.18.18.101:9092,172.18.18.102:9092",
             "group.id" -> "FaceObjectConsumerGroup"
         )
-        val topicsSet = "feaure".split()toSet
+        val topicsSet = "first".split(",").toSet
         val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-        val messaget = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-        KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
-//        val conf = new SparkConf().setMaster("local[*]").setAppName("SparkStreamingOnKafkaDirect")
-//        val ssc = new StreamingContext(conf, Durations.seconds(15))
-//        ssc.checkpoint("/checkpoint")
-//        val kafkaParams = Map(
-//            "metadata.broker.list" -> "172.18.18.100:9092,172.18.18.101:9092,172.18.18.102:9092",
-//            "group.id" -> "FaceObjectConsumerGroup"
-//        )
-//
-//        val topicsSet = "feature".split(",").toSet
-//
-//        val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
+        messages.foreachRDD(rdd => {
+            rdd.foreach(row => {println("key: " + row._1 + ", "  + "value: " + row._2)})
+        })
+
+        ssc.start()
+        ssc.awaitTermination()
     }
 }
